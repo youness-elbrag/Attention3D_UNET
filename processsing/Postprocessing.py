c74daf46426db690d66b9e8c4dbc57e974ca3275
@Youness_Elbrag <younsselbrag@gmail.com>
2022-10-10 22:11:27 +0300

    create class object for tool

--- processsing/Postprocessing.py
+++ processsing/Postprocessing.py
@@ -1,71 +1,86 @@
 import argparse
-from nipype.interfaces.ants import N4BiasFieldCorrection
 from scipy import ndimage as nd
 import ants
 
-import nipype.interfaces.fsl as fsl
-from nipype.interfaces.fsl import BET, IsotropicSmooth
-from Posthelper import *
 from pathlib import Path
+import shutil
+import SimpleITK as sitk
+import glob
+import os
+from processed_data import Normalize_Brastdata
+modalities = ["flair", "t1", "t1ce", "t2"]
 input_file_data='BraTS2020_TrainingData'
-#output_file_data_corrected_n4bais='/content/BraTS2020_TrainingData_correted_N4Bias'
-
-error_list = []
+output_file_data='BraTS2020_TrainingDataCorrected/'
+#outpuerror_list = []
 Normlize_VS = [1, 1, 1]
 
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser("--help",description='tool for MRI artifics using python Python '
                                                     'Procssing dataset Brats2020 ')
-parser.add_argument('--path', metavar='ENV_DIR', nargs='+',
+parser.add_argument('--path',required=True,
                         help='A directory to create the environment in.')                                               
 parser.add_argument('--n4baisfieldcorrection',action='store_true',
 help="A directory to save corrected samples n4baisfieldcorrection ")
 parser.add_argument('--skull_stripping',action='store_true',
 help="A directory to save corrected samples Skull stripping.")
-
 args = parser.parse_args() 
 
-def n4baisfieldcorrection(in_path, out_path, image_type=sitk.sitkFloat64):
-    # N. Tustison et al., N4ITK: Improved N3 Bias Correction, IEEE Transactions on Medical Imaging, 29(6):1310-1320, June 2010.
-  
-      input_image = ants.image_read(in_path)
-      ants.n4_bias_field_correction(input_image).to_file(out_path)
-      #sitk.WriteImage(output_image, out_path)
-      return os.path.abspath(out_path)
 
-def skull_stripping(in_path, out_path, image_type=sitk.sitkFloat64):
-    # N. Tustison et al., N4ITK: Improved N3 Bias Correction, IEEE Transactions on Medical Imaging, 29(6):1310-1320, June 2010.
-    try:
-        BET(in_file=in_path,
-            out_file = out_path).run()
-        
-    except:
-        error_list.append(out_path)
-        print(error_list)
 
-        pass
-      #sitk.WriteImage(output_image, out_path)
-    return os.path.abspath(out_path)  
+def get_image_path(subject_folder, name):
+    file_name = os.path.join(subject_folder, "*" + name + ".nii")
+    return glob.glob(file_name)[0]
+
+def check_origin(in_path, in_path2):
+    image = sitk.ReadImage(in_path)
+    image2 = sitk.ReadImage(in_path2)
+    if not image.GetOrigin() == image2.GetOrigin():
+        image.SetOrigin(image2.GetOrigin())
+        sitk.WriteImage(image, in_path)
         
-def normalize_image(in_path, out_path, correction=True):
-    if correction and args.n4baisfieldcorrection:
-        n4baisfieldcorrection(in_path, out_path)
-    else:
-        shutil.copy(in_path, out_path)
-    
-    if correction and args.skull_stripping:
-        skull_stripping(in_path, out_path)  
-    else:
-        shutil.copy(in_path, out_path)
+
+def preprocess_brats_folder(in_folder, out_folder, truth_name='seg', no_correction_modalities=None):
+    for name in modalities:
+        image_image = get_image_path(in_folder, name)
+        case_ID = os.path.basename(out_folder)
+        out_path = os.path.abspath(os.path.join(out_folder, "%s_%s.nii"%(case_ID, name)))
+        perform_correction = no_correction_modalities and name not in no_correction_modalities
+        normalize_image=Normalize_Brastdata(image_image, out_folder,correction=perform_correction)
+        if args.n4baisfieldcorrection:
+           normalize_image(args.n4baisfieldcorrection)
+        elif args.skull_stripping:
+
+           normalize_image(args.skull_stripping)
+        else: 
+            print("command isn't corrected try again")    
+
+    truth_image = get_image_path(in_folder, truth_name)
+    out_path = os.path.abspath(os.path.join(out_folder, "%s_seg.nii"%(case_ID)))
+    shutil.copy(truth_image, out_path)
+    check_origin(out_path, get_image_path(in_folder, modalities[0])) # check with the flair image
+
+def preprocess_brats_data(brats_folder, out_folder, overwrite=False, no_correction_modalities=("flair")):
+    for subject_folder in glob.glob(os.path.join(brats_folder, "*", "*")):
+        if os.path.isdir(subject_folder):
+            subject = os.path.basename(subject_folder)
+            new_subject_folder = os.path.join(out_folder, os.path.basename(os.path.dirname(subject_folder)),
+                                              subject)
+            if not os.path.exists(new_subject_folder) or overwrite:
+                if not os.path.exists(new_subject_folder):
+                    os.makedirs(new_subject_folder)
+                preprocess_brats_folder(subject_folder, new_subject_folder,
+                                     no_correction_modalities=no_correction_modalities)
+
+
 
 def main(brats_path, preprocessed_brats):
     preprocess_brats_data(brats_path, preprocessed_brats)
 
 if __name__ == "__main__":                   
-    if args.path and args.n4baisfieldcorrection:
-        main(brats_path=input_file_data, preprocessed_brats=str(args.path))
-    elif args.path and args.skull_stripping:
-              main(brats_path=input_file_data, preprocessed_brats=str(args.path))
+    if args.path :
+        main(brats_path=input_file_data, preprocessed_brats=output_file_data+args.path)
+    elif args.path :
+        main(brats_path=input_file_data, preprocessed_brats=output_file_data+args.path)
     else:
         print("please inster the full agrument CLI tool") 
     
